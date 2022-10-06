2e72fabd8cfedb3d28cfc78e2f0ed145d68d4263
Varga Tímea <18d_vargat@NYIRSZIKSZI.local>
2022-10-06 13:48:01 +0200

    Initial commit

---
+++ src/jarmu/Jarmu.java
@@ -0,0 +1,35 @@
+package jarmu;
+
+public abstract class Jarmu {
+
+    private String rendszam;
+    private int aktSebesseg;
+
+    public Jarmu(String rendszam, int aktSebesseg) {
+        this.rendszam = rendszam;
+        this.aktSebesseg = aktSebesseg;
+    }
+
+    public abstract boolean gyorshajtottE(int sebessegkorlat) ;
+
+    public String getRendszam() {
+        return rendszam;
+    }
+
+    public void setRendszam(String rendszam) {
+        this.rendszam = rendszam;
+    }
+
+    public int getAktSebesseg() {
+        return aktSebesseg;
+    }
+
+    public void setAktSebesseg(int aktSebesseg) {
+        this.aktSebesseg = aktSebesseg;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(" %s - %d km/h", this.rendszam, this.aktSebesseg);
+    }
+}
