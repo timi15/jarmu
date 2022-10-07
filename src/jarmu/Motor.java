535d38d496ff3e73aa26ed55abeb8c5f52c53557
18d_solteszm <solteszmonika7@gmail.com>
2022-10-07 08:46:22 +0200

    Motor class

---
+++ src/jarmu/Motor.java
@@ -0,0 +1,38 @@
+package jarmu;
+
+public class Motor extends  Jarmu{
+    private int maxSebesseg;
+    private String marka;
+
+    public Motor(String rendszam, int aktSebesseg, int maxSebesseg, String marka) {
+        super(rendszam, aktSebesseg);
+        this.maxSebesseg = maxSebesseg;
+        this.marka = marka;
+    }
+
+    public int getMaxSebesseg() {
+        return maxSebesseg;
+    }
+
+    public void setMaxSebesseg(int maxSebesseg) {
+        this.maxSebesseg = maxSebesseg;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    @Override
+    public boolean gyorshajtottE(int sebessegkorlat) {
+        return this.aktSebesseg > sebessegkorlat;
+    }
+
+    @Override
+    public String toString() {
+        return "Motor: "+super.toString();
+    }
+}
